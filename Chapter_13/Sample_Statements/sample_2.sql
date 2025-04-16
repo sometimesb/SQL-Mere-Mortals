e84cacb66d5c4f679ea62652c0aad1d598c7be13
sometimesb <77695101+sometimesb@users.noreply.github.com>
2025-04-15 19:50:18 -0500

    SQL Problem

--- Chapter_13/Sample_Statements/sample_2.sql
+++ Chapter_13/Sample_Statements/sample_2.sql
@@ -1,8 +1,13 @@
 USE EntertainmentAgencyExample
 
-SELECT Entertainers.EntertainerID, Members.MbrFirstName, Members.MbrLastName, Entertainer_Members.Status
+SELECT Entertainers.EntertainerID, Members.MbrFirstName, Members.MbrLastName,
+    SUM(Engagements.ContractPRice)/ (SELECT COUNT(*) FROM Entertainer_Members WHERE Entertainer_Members.Status <> 3 AND Entertainer_Members.EntertainerID=Entertainers.EntertainerID) AS memberPay
 FROM Entertainers
 INNER JOIN [Entertainer_Members] ON Entertainer_Members. EntertainerID = Entertainers.EntertainerID
 INNER JOIN Members ON Members.MemberID = Entertainer_Members.MemberID
 INNER JOIN Engagements ON Engagements.EntertainerID = Entertainers.EntertainerID
-WHERE Entertainer_Members.Status = 999
+
+WHERE Entertainer_Members.Status <> 3
+
+GROUP BY Entertainers.EntertainerID, Members.MbrFirstName, Members.MbrLastName
+ORDER BY Members.MbrLastName, Entertainers.EntertainerID
