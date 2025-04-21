393e62e1bc3b1b8fb903d4b0bd994f9dca1fd728
sometimesb <77695101+sometimesb@users.noreply.github.com>
2025-04-21 11:15:11 -0500

    SQL Problem

---
+++ Chapter_18/chapter_statements/CH18_Recipes_NOT_Beef_Onion_Carrot_NOTIN_1.sql
@@ -0,0 +1,18 @@
+--find recipes that have neither beef nor onions nor carrots with not in using 3 seperate not in clauses
+SELECT Recipes.RecipeTitle
+FROM Recipes
+WHERE Recipes.RecipeID  NOT IN
+	(SELECT Recipe_Ingredients.RecipeID
+	FROM Recipe_Ingredients
+	INNER JOIN Ingredients ON Ingredients.IngredientID = Recipe_Ingredients.IngredientID
+	WHERE Ingredients.IngredientName = 'Beef')
+AND Recipes.RecipeID  NOT IN
+	(SELECT Recipe_Ingredients.RecipeID
+	FROM Recipe_Ingredients
+	INNER JOIN Ingredients ON Ingredients.IngredientID = Recipe_Ingredients.IngredientID
+	WHERE Ingredients.IngredientName = 'Onion')
+AND Recipes.RecipeID  NOT IN
+	(SELECT Recipe_Ingredients.RecipeID
+	FROM Recipe_Ingredients
+	INNER JOIN Ingredients ON Ingredients.IngredientID = Recipe_Ingredients.IngredientID
+	WHERE Ingredients.IngredientName = 'Carrot')
